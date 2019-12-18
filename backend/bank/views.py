d341e9ddbd62e59323bb3ebc67d40614e7c14769
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2019-12-18 11:58:03 -0800

    added: viewsSerializers

--- backend/bank/views.py
+++ backend/bank/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group, Account, Customer, Product
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, AccountSerializer, CustomerSerializer, ProductSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializers
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializers
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializers
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
