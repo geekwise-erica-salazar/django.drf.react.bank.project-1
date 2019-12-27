0acab727a3e71b357b79954e214fa0547d7a527b
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2019-12-27 13:46:55 -0800

    update

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
       return;
     }
     axios
-      .post("https://vs-code-12-16-19", item)
+      .post("https://vs-code-12-16-19.herokuapp.com/", item)
       .then(res => this.refreshList());
   };
   handleDelete = item => {
