3b73775fc3893678c2fa788804d855e058e89928
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2019-12-30 13:44:24 -0800

    added:views

--- backend/bank/views.py
+++ backend/bank/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from bank.models import Branch, Account, Customer, Product
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, BranchSerializer, AccountSerializer, CustomerSerializer, ProductSerializer
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.6
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+#########
+class BranchViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
