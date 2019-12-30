dee30dad5c5c25bf678bc087f1a39b8e0fb23b25
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2019-12-30 13:48:09 -0800

    added:rest_fameworks to setting

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,6 +32,11 @@ ALLOWED_HOSTS = [
     'vs-code-12-16-19.herokuapp.com'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Application definition
 
@@ -41,6 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+    'bank',
+
 ]
 
 MIDDLEWARE = [
