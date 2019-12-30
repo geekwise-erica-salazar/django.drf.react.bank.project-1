eec6ea1b4a012525679bb95e4ea5f8041c65d3fd
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2019-12-30 13:53:12 -0800

    updated:settings/cors_origin

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,5 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 django_heroku.settings(locals())
+
+CORS_ORIGIN_WHITELIST = (
+         'https://front-vs-12-17-19.herokuapp.com',
+     )
