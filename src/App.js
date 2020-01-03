a1cf3542ee6bafa9d09080ca9a695391b01937b8
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2020-01-02 16:36:08 -0800

    updated:modules

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ class App extends Component {
     //   item => item.completed === viewCompleted
     // );
     const newItems=this.state.bankApp
-    return newItems.map(item => (
+    return data.newItems.map(item => (
       <li
         key={item.id}
         className="list-group-item d-flex justify-content-between align-items-center"
