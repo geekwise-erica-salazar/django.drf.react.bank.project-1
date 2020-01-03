c03e7b7440f6e521f6f72df10b65721c1e6c1b69
geekwise-jesus-garcia <jesus1287garcia@yahoo.com>
2020-01-02 16:17:36 -0800

    updated:modules

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   refreshList = () => {
     axios
       .get("https://vs-code-12-16-19.herokuapp.com/branch/")
-      .then(res => this.setState({ bankApp: res.data }))
+      .then(res => this.setState({ bankApp: res.data.results }))
       .catch(err => console.log(err));
   };
   displayCompleted = status => {
